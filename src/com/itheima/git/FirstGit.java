8ed2bf70606f19fd0513f443a2b7f0fe49354958
liyang <liyang@itcast.cn>
2018-11-27 09:40:29 +0800

    2

--- src/com/itheima/git/FirstGit.java
+++ src/com/itheima/git/FirstGit.java
@@ -1,7 +1,14 @@
 package com.itheima.git;
 
+import org.junit.Test;
+
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
+    }
+
+    @Test
+    public void add(){
+        System.out.println("hello,git2...");
     }
 }
